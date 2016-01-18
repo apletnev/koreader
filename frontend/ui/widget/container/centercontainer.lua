fdde2931cbaf9f831cdb7499dc5cc9a600cb4778
Alex Pletnev <pletnev.alex@gmail.com>
2016-01-18 17:37:43 +0200

    #1730 Create complete book feature Add margin and padding into containers for debugging purpose

--- frontend/ui/widget/container/centercontainer.lua
+++ frontend/ui/widget/container/centercontainer.lua
@@ -8,6 +8,8 @@ local CenterContainer = WidgetContainer:new({
     bordersize = 0,
     color = Blitbuffer.COLOR_BLACK,
     radius = 0,
+    padding = 0,
+    margin = 0,
 })
 
 function CenterContainer:paintTo(bb, x, y)
@@ -16,6 +18,10 @@ function CenterContainer:paintTo(bb, x, y)
         -- throw error? paint to scrap buffer and blit partially?
         -- for now, we ignore this
     end
+
+    local content_size_width = content_size.w + (self.padding + self.margin + self.bordersize) * 2
+    local content_size_height = content_size.h + (self.padding + self.margin + self.bordersize) * 2
+
     local x_pos = x
     local y_pos = y
     if self.ignore ~= "height" then
@@ -26,13 +32,15 @@ function CenterContainer:paintTo(bb, x, y)
     end
 
     if self.bordersize > 0 then
-        bb:paintBorder(x_pos, y_pos,
-            content_size.w,
-            content_size.h,
+        bb:paintBorder(x_pos + self.margin, y_pos + self.margin,
+            content_size_width - self.margin,
+            content_size_height - self.margin,
             self.bordersize, self.color, self.radius)
     end
     if self[1] then
-        self[1]:paintTo(bb, x_pos, y_pos)
+        self[1]:paintTo(bb,
+            x_pos + self.padding + self.margin + self.bordersize,
+            y_pos + self.padding + self.margin + self.bordersize)
     end
 end
 
